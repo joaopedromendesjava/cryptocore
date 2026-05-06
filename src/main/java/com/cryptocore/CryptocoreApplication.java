0be185a73ceacf385c845bef67799356e7a31b68
joaopedromendesjava <joaopedromendessilva@gmail.com>
2026-05-06 20:28:18 -0300

    feat: add initial commit

---
+++ src/main/java/com/cryptocore/CryptocoreApplication.java
@@ -0,0 +1,13 @@
+package com.cryptocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptocore/CryptocoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
